06d42ed9821f110a3e321b40f9ea14dee0cd16d5
Harsh2003-oss <harsh.sil872@gmail.com>
2026-02-04 18:33:45 +0530

    fix: bug  fixed

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const {userAuth }= require('../middlewares/auth')
 const ConnectionRequest = require('../models/connectionRequest')
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills"
 
 userRouter.get("/user/requests/received",userAuth,async (req,res) => {
     try {
@@ -19,6 +20,42 @@ userRouter.get("/user/requests/received",userAuth,async (req,res) => {
 
     } catch (error) {
           res.status(400).send(error.message)
+    }
+})
+
+userRouter.get('/user/connections',userAuth , async (req,res) => {
+    try {
+        const loggedInUser = req.user;
+    
+        const connectionRequests = await ConnectionRequest.find({
+
+            $or: [
+                {toUserId:loggedInUser._id, status:"accepted"},
+                {fromUserId:loggedInUser._id, status :"accepted"}
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId",USER_SAFE_DATA);
+
+        console.log(connectionRequests)
+
+        const data = connectionRequests.map((row)=>{
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.fromUserId;
+            }else {
+    return row.fromUserId;
+  }
+        })
+
+       
+    res.json({data})
+      console.log(connectionRequests)
+
+    
+    } 
+
+    catch (error) {
+         res.status(400).send(error.message)
     }
 })
 
